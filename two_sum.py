5c3c5d5a1fd8da789f33ccbd6ae5830aab957966
Mustafa Bulut <bulutmustafa.0134@gmail.com>
2024-03-04 00:14:06 +0300

    duplicate data cleared and The question named two sum is over

--- two_sum.py
+++ two_sum.py
@@ -1,15 +1,22 @@
-nums = [2,7,11,15]
-target = 9
+nums = [3, 9, 2, 4, 7]
+target = 11
 def two_sum(arrays):
     target_list = []
     for i in arrays:
         x,y = 0,True
         while y:
             if i + arrays[x] == target:
-                target_list.append([i,arrays[x]])
+                params = [i,arrays[x]]
+                params.sort()
+                target_list.append(params)
             if x == len(arrays)-1:
                 y = False
             x +=1
+    for index,params in enumerate(target_list):
+        if target_list.count(params) > 1:
+            target_list.pop(index)
     print(target_list)
+        
+
 
 two_sum(nums)
