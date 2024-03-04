f5d16b1ef144a4603ffca46429f109081883194a
Mustafa Bulut <bulutmustafa.0134@gmail.com>
2024-03-04 10:44:22 +0300

    negative numbers processed

--- Reverse_Integer.py
+++ Reverse_Integer.py
@@ -1,8 +1,11 @@
-x = 1230000
+x = -2020
+po_ne = lambda num: num > 0
 def reverse_int(num):
+    positive = po_ne(num)
     num,a = str(num),""
     num = list(num)
     num.reverse()
+    num.remove("-")
     num_1 = num[:]
     for x in num_1:
         if x == "0":
@@ -11,15 +14,13 @@ def reverse_int(num):
             break
     for i in num:
         a+=i
-    return int(a)
+    a = int(a)
+    if positive is False:
+        a *=-1
         
-    
-    
-            
+    return a
         
-
-    
-        
+   
 print(reverse_int(x))
 
 
