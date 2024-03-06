9446454e7d44e9b1c33b50e0d5e2c426946830dd
Mustafa Bulut <bulutmustafa.0134@gmail.com>
2024-03-06 23:30:26 +0300

    the Code little ımproved and Improved code from chatgbt was written

--- Reverse_Integer.py
+++ Reverse_Integer.py
@@ -1,11 +1,11 @@
-x = -2020
-po_ne = lambda num: num > 0
+x = 2020
+pos_or_neg = lambda num: num > 0
 def reverse_int(num):
-    positive = po_ne(num)
-    num,a = str(num),""
-    num = list(num)
-    num.reverse()
-    num.remove("-")
+    positive = pos_or_neg(num)
+    num,result = list(str(num)),""
+    num = num[::-1]
+    if positive  == 0:
+        num.remove("-")
     num_1 = num[:]
     for x in num_1:
         if x == "0":
@@ -13,14 +13,31 @@ def reverse_int(num):
         else:
             break
     for i in num:
-        a+=i
-    a = int(a)
+        result+=i
     if positive is False:
-        a *=-1
-        
-    return a
-        
+        result = "-" +result
+    return result
    
+print(reverse_int(x))
+
+#* Improved code from chatgbt
+def reverse_int(num):
+    # Sayının işaretini kontrol et
+    is_positive = num > 0
+    # Sayıyı mutlak değere çevir
+    num = abs(num)
+    # Sayıyı dizeye dönüştür ve tersine çevir
+    num_str = str(num)[::-1]
+    # Tersine çevrilen sayıda önemli sıfırları kaldır
+    num_str = num_str.lstrip("0")
+    # Negatif sayıyı tekrar ekle
+    if not is_positive:
+        num_str = "-" + num_str
+    # Sonucu döndür
+    return int(num_str)
+
+# Örnek kullanım
+x = -2020
 print(reverse_int(x))
 
 
