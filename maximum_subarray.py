7357614b4e5c3e52c8c144047437fc308ebd4205
Mustafa <bulutmustafa.0134@gmail.com>
2024-03-31 04:27:09 +0300

    Chatgbt`s codes writed

--- maximum_subarray.py
+++ maximum_subarray.py
@@ -1,5 +1,7 @@
+
+#region#*My Codes
 nums = [5, -3, 7, -8, 2, 1, -9, 10]
-x = [1, 2, 3, 4, 5]
+x = [-1, -2, -3, -4, -5]
 def lists(num,arrays):
     first_int ,last_int,params,x= 0, num,[],len(arrays)+1
     while last_int <= x :
@@ -21,3 +23,21 @@ def consecutive(arrays):
 
 print(consecutive(x))
 print(consecutive(nums))
+#endregion
+
+#region#*Chatgbt`s codes
+def maxSubArray(nums):
+    max_sum = current_sum = nums[0]
+    
+    for num in nums[1:]:
+        current_sum = max(num, current_sum + num)
+        max_sum = max(max_sum, current_sum)
+    
+    return max_sum
+
+# Örnek kullanım
+nums = [5, -3, 7, -8, 2, 1, -9, 10]
+result = maxSubArray(nums)
+print(result)
+
+#endregion
