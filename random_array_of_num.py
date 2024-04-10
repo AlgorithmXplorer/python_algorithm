56e87cce27ebdfcf2027481e3af4d6b022662323
Mustafa <bulutmustafa.0134@gmail.com>
2024-04-10 16:43:53 +0300

    the module has fixed

--- random_array_of_num.py
+++ random_array_of_num.py
@@ -3,4 +3,7 @@ def random_array():
     first_num,last_num,step = int(input("liste için ilk sayı: ")),int(input("son sayı: ")), int(input("adım sayısı: "))
     array = list(range(first_num,last_num,step))
     adet = int(input("kaç adet sayı olsun: "))
-    return random.sample(array,adet)
+    nums = []
+    for i in range(adet):
+        nums.append(random.choice(array))
+    return nums
