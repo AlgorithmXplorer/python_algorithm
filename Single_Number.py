1ede2dfe8f7b85e8a276fec2b3ce23935b679d21
Mustafa <bulutmustafa.0134@gmail.com>
2024-04-10 17:00:18 +0300

    Single Number question has over

--- Single_Number.py
+++ Single_Number.py
@@ -2,15 +2,11 @@ import random_array_of_num
 nums = random_array_of_num.random_array()
 def Single_Num(arrays):
     single_nums = []
-    not_single_nums = []
     for i in arrays:
         if arrays.count(i) == 1:
             single_nums.append(i)
-        else:
-            not_single_nums.append(i)
+    return single_nums
 
-    print(single_nums)
-    print(not_single_nums)
 
-print(nums)
-Single_Num(nums)
+
+print(f"list: {nums}\nsingle numbers: {Single_Num(nums)}")
