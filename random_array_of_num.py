3cae02cbc555d5f29156f29cf7ca3c9b4b84538f
Mustafa <bulutmustafa.0134@gmail.com>
2024-04-13 13:54:02 +0300

    module is now built in

--- random_array_of_num.py
+++
@@ -1,9 +0,0 @@
-import random
-def random_array():
-    first_num,last_num,step = int(input("liste için ilk sayı: ")),int(input("son sayı: ")), int(input("adım sayısı: "))
-    array = list(range(first_num,last_num,step))
-    adet = int(input("kaç adet sayı olsun: "))
-    nums = []
-    for i in range(adet):
-        nums.append(random.choice(array))
-    return nums
