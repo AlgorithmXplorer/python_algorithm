7aa1db8a3b1d727a6fd2f9fee4f9dd05c65d684e
Mustafa <bulutmustafa.0134@gmail.com>
2024-04-19 23:37:04 +0300

    question called shhufle the array is over

--- Shuffle_the_Array.py
+++ Shuffle_the_Array.py
@@ -1,23 +1,23 @@
 
 nums = [1,3,2,4,5,9,2,5,7,3]
-def Shuffle_the_array(lst):
-    index = int(input("index: "))
+def Shuffle_the_array(lst,index):
     list_1 = lst[:index]
     list_2 = lst[index:]
-    print(list_1,list_2)
     x = len(list_2) >= len(list_1)
-    list_1yedek = list_1[::]
-    list_2yedek = list_2[::]
+    last_list = []
     if x:
-        for index,num in enumerate(list_1):
-            list_1yedek.insert(list_1yedek.index(num)+1 , list_2[index])
-        # listee = list_2[len(list_1):] 
-        # list_1yedek += listee
-        print(list_1yedek)
+        for index,nums in enumerate(list_1):
+            last_list.append(nums)
+            last_list.append(list_2[index])
+        x = list_2[len(list_1):]
+        last_list += x
 
+    else:
+        for index,nums in enumerate(list_2):
+            last_list.append(list_1[index])
+            last_list.append(nums)
+        x = list_1[len(list_2):]
+        last_list += x
+    return last_list
 
-
-
-2,1,5,3,7,2,3,46
-
-Shuffle_the_array(nums)
+print(Shuffle_the_array(nums,3))
