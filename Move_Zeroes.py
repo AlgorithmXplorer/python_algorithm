84ee8c66567524d59c9ee6969eb5b21defe1f1e2
Mustafa <bulutmustafa.0134@gmail.com>
2024-04-23 01:04:45 +0300

    chatgbt's code is writted

--- Move_Zeroes.py
+++ Move_Zeroes.py
@@ -1,3 +1,5 @@
+
+#region#*My_code
 import random_array_of_num
 nums = random_array_of_num.random_array()
 
@@ -9,3 +11,25 @@ def Move_zeroes(numbers):
 
     return numbers
 Move_zeroes(nums)
+#endregion
+
+#region#*Chatgbt's_code
+def moveZeroes(nums):
+    zero_index = 0
+    
+    # Sıfır olmayan elemanları öne taşı
+    for i in range(len(nums)):
+        if nums[i] != 0:
+            nums[zero_index], nums[i] = nums[i], nums[zero_index]
+            zero_index += 1
+    
+    return nums
+
+# Örnek kullanım
+nums = [0, 1, 0, 3, 12]
+result = moveZeroes(nums)
+print(result)
+
+#endregion
+
+
