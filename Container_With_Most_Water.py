35e3ca378a4e2f2076609b0e3ee32fbb3c5aae93
Mustafa <bulutmustafa.0134@gmail.com>
2025-01-11 20:41:01 +0300

    the solution of the Container_With_Most_Water question is complete

--- Container_With_Most_Water.py
+++ Container_With_Most_Water.py
@@ -1,8 +1,26 @@
 height =[10, 14, 10, 4, 10, 14, 10]
 
 def main(array):
-    def slicing(param):
-        pass
+    def slicing(param):       
+        slice_list = []
+        value_list =[]
+        for index,num in enumerate(param,1):
+            for indx,number in enumerate(param,1):
+                lst = [num,number]
+                slice_list.append(lst)
+                value = abs(index-indx) * min(lst)
+                value_list.append(value)
+
+
+        return [slice_list,value_list]
+    dual_list = slicing(array)
+    return max(dual_list[1])    
+
+
+
+    
+
+            
 
 
 
